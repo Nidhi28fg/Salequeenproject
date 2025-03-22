ea5c3eb4cf2c65c590db051f5c66995342921d10
Nidhi Singh Bhadoria <teamguffonidhi28@gmail.com>
2025-03-22 08:37:57 +0530

    change onclick function Project

---
+++ Kanban/code/script.js
@@ -0,0 +1,26 @@
+function handleAddBtn(id){
+    appendNew(id,document.getElementById(`title${id}`).value,document.getElementById(`title${id}`),1)
+}
+
+
+document.getElementById('title1')?.addEventListener("keydown",(e)=>{
+    if (e.key === 'Enter') { handleAddBtn(1)}
+})
+
+
+document.getElementById('title2')?.addEventListener("keydown",(e)=>{
+    if (e.key === 'Enter') { handleAddBtn(2)}
+})
+
+document.getElementById('title3')?.addEventListener("keydown",(e)=>{
+    if (e.key === 'Enter') { handleAddBtn(3)}
+})
+
+document.getElementById('title4')?.addEventListener("keydown",(e)=>{
+    if (e.key === 'Enter') { handleAddBtn(4)}
+})
+
+document.getElementById('title5')?.addEventListener("keydown",(e)=>{
+    if (e.key === 'Enter') { handleAddBtn(5)}
+})
+
